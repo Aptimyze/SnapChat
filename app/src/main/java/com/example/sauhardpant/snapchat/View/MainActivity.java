3aa1d6d72451966a530b37c31792a257df25ced9
tjarpsp <tjarpsp@gmail.com>
2018-12-12 22:05:50 -0500

    Implemented viewpager

--- app/src/main/java/com/example/sauhardpant/snapchat/View/MainActivity.java
+++ app/src/main/java/com/example/sauhardpant/snapchat/View/MainActivity.java
@@ -1,15 +1,57 @@
 package com.example.sauhardpant.snapchat.View;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.sauhardpant.snapchat.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    ViewPagerAdapter viewPagerAdapter;
+    @BindView(R.id.activity_main_viewpager)
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
+        viewPager.setCurrentItem(1);
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            switch (i) {
+                case 0:
+                    return new ChatFragment();
+                case 1:
+                    return new CameraFragment();
+                case 2:
+                    return new StoryFragment();
+                default:
+                    return new CameraFragment();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
     }
 }
