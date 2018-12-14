79090142974b62b0e1b4395fa12f2b8d55b76771
tjarpsp <tjarpsp@gmail.com>
2018-12-13 20:55:22 -0500

    Camera API

--- app/src/main/java/com/example/sauhardpant/snapchat/View/CameraFragment.java
+++ app/src/main/java/com/example/sauhardpant/snapchat/View/CameraFragment.java
@@ -1,19 +1,91 @@
 package com.example.sauhardpant.snapchat.View;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.sauhardpant.snapchat.R;
 
-public class CameraFragment extends Fragment {
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CameraFragment extends Fragment implements SurfaceHolder.Callback {
+
+    private static final int CAMERA_REQUEST_CODE = 123;
+
+    Camera camera;
+    SurfaceHolder surfaceHolder;
+    @BindView(R.id.fragment_camera_surfaceView)
+    SurfaceView surfaceView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_camera, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_camera, container, false);
+        ButterKnife.bind(this, rootView);
+
+        surfaceHolder = surfaceView.getHolder();
+
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+        } else {
+            surfaceHolder.addCallback(this);
+            surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        }
+
+
+        return rootView;
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                surfaceHolder.addCallback(this);
+                surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+            } else {
+                Toast.makeText(getContext(), "Need Camera Permission", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        camera = Camera.open();
+
+        // need to change the default way the camera gives your the image
+        camera.setDisplayOrientation(90);
+        Camera.Parameters parameters = camera.getParameters();
+        parameters.setPreviewFrameRate(30);
+        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+        camera.setParameters(parameters);
+        try {
+            camera.setPreviewDisplay(holder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        camera.startPreview();
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {}
 }
